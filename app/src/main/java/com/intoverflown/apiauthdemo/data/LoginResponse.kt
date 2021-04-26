ae8ebe400daa49cc88a76600aa24f11ce2bb5c9d
RocqJones <jonesmbindyo@gmail.com>
2021-04-26 15:30:43 +0300

    api data class: Response params

---
+++ app/src/main/java/com/intoverflown/apiauthdemo/data/LoginResponse.kt
@@ -0,0 +1,10 @@
+package com.intoverflown.apiauthdemo.data
+
+data class LoginResponse(
+    val FirstName: String,
+    val Id: String,
+    val LastName: String,
+    val Role: String,
+    val Token: String,
+    val Username: String
+)
