e13a1b3d5c53d4972c9c1f04726e85a74d90f280
RocqJones <jonesmbindyo@gmail.com>
2021-04-26 15:31:50 +0300

    api interface

--- app/src/main/java/com/intoverflown/apiauthdemo/network/ApiInterface.kt
+++ app/src/main/java/com/intoverflown/apiauthdemo/network/ApiInterface.kt
@@ -1,4 +1,11 @@
 package com.intoverflown.apiauthdemo.network
 
+import com.intoverflown.apiauthdemo.data.LoginResponse
+import retrofit2.Response
+import retrofit2.http.POST
+
 interface ApiInterface {
+
+    @POST("Service/POS/API/v1/Connect/Login")
+    suspend fun login() : Response<LoginResponse>
 }
