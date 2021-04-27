123a2be0d77d11aaacf7742436fd3702a0316932
RocqJones <jonesmbindyo@gmail.com>
2021-04-27 16:12:43 +0300

    Old: base repository

--- app/src/main/java/com/intoverflown/apiauthdemo/repository/BaseRepository.kt
+++ app/src/main/java/com/intoverflown/apiauthdemo/repository/BaseRepository.kt
@@ -1,32 +1,27 @@
 package com.intoverflown.apiauthdemo.repository
 
-import com.intoverflown.apiauthdemo.network.ApiResponseHandler
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import retrofit2.HttpException
-
 abstract class BaseRepository {
-    suspend fun <T> safeApiCall(
-        apiCall : suspend () -> T
-    ) : ApiResponseHandler<T> {
-        // execute all api calls in IO dispatcher
-        return withContext(Dispatchers.IO) {
-            // check api calls
-            try {
-                // if successful we'll get results directly and put them in our ApiResponseHandler
-                ApiResponseHandler.Success(apiCall.invoke())
-            } catch (throwable : Throwable) {
-                when (throwable) {
-                    // api error
-                    is HttpException -> {
-                        ApiResponseHandler.Failure(false, throwable.code(), throwable.response()?.errorBody())
-                    }
-                    // considered as network error
-                    else -> {
-                        ApiResponseHandler.Failure(true, null, null)
-                    }
-                }
-            }
-        }
-    }
+//    suspend fun <T> safeApiCall(
+//        apiCall : suspend () -> T
+//    ) : ApiResponseHandler<T> {
+//        // execute all api calls in IO dispatcher
+//        return withContext(Dispatchers.IO) {
+//            // check api calls
+//            try {
+//                // if successful we'll get results directly and put them in our ApiResponseHandler
+//                ApiResponseHandler.Success(apiCall.invoke())
+//            } catch (throwable : Throwable) {
+//                when (throwable) {
+//                    // api error
+//                    is HttpException -> {
+//                        ApiResponseHandler.Failure(false, throwable.code(), throwable.response()?.errorBody())
+//                    }
+//                    // considered as network error
+//                    else -> {
+//                        ApiResponseHandler.Failure(true, null, null)
+//                    }
+//                }
+//            }
+//        }
+//    }
 }
