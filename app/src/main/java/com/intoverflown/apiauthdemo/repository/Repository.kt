52c9cb9ed62cc06130b998f304204f263f5ea912
RocqJones <jonesmbindyo@gmail.com>
2021-04-27 16:12:29 +0300

    Old: Repository

---
+++ app/src/main/java/com/intoverflown/apiauthdemo/repository/Repository.kt
@@ -0,0 +1,8 @@
+package com.intoverflown.apiauthdemo.repository
+
+class Repository {
+
+//    fun login(username : String, password : String) : Call<LoginResponse> {
+//        return ApiClient.api.login(username, password)
+//    }
+}
