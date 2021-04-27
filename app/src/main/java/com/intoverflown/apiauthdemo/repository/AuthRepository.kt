fe16c6708397a21c74fa4c0e2e2dda2f22346762
RocqJones <jonesmbindyo@gmail.com>
2021-04-27 16:12:52 +0300

    Old: auth repository

--- app/src/main/java/com/intoverflown/apiauthdemo/repository/AuthRepository.kt
+++ app/src/main/java/com/intoverflown/apiauthdemo/repository/AuthRepository.kt
@@ -2,10 +2,10 @@ package com.intoverflown.apiauthdemo.repository
 
 import com.intoverflown.apiauthdemo.network.ApiInterface
 
-class AuthRepository(private val api : ApiInterface) : BaseRepository() {
-
-    // login
-    suspend fun login(username: String, password : String) = safeApiCall {
-        api.login(username, password)
-    }
-}
+//class AuthRepository(private val api : ApiInterface) : BaseRepository() {
+//
+//    // login
+//    suspend fun login(username: String, password : String) = safeApiCall {
+//        api.login(username, password)
+//    }
+//}
