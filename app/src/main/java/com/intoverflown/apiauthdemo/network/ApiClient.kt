55f2c6b733dd860cacbb2ae0861fc1df562641ac
RocqJones <jonesmbindyo@gmail.com>
2021-04-27 16:13:09 +0300

    api interface

--- app/src/main/java/com/intoverflown/apiauthdemo/network/ApiClient.kt
+++ app/src/main/java/com/intoverflown/apiauthdemo/network/ApiClient.kt
@@ -1,29 +1,35 @@
 package com.intoverflown.apiauthdemo.network
 
-import androidx.viewbinding.BuildConfig
-import com.intoverflown.apiauthdemo.utils.Constants.Companion.BASE_URL
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+object ApiClient {
 
-class ApiClient {
+    // RECENT
+//    private val retrofit by lazy {
+//        Retrofit.Builder()
+//                .baseUrl(BASE_URL)
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build()
+//    }
+//
+//    val api: ApiInterface by lazy {
+//        retrofit.create(ApiInterface::class.java)
+//    }
 
-    fun <Api> buildApi(
-        api: Class<Api>
-    ) : Api {
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .client(OkHttpClient.Builder().also { client ->
-                if (BuildConfig.DEBUG) {
-                    val logging = HttpLoggingInterceptor()
-                    logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-                    client.addInterceptor(logging)
-                }
-            }.build())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(api)
-    }
+    // OLD
+//    fun <Api> buildApi(
+//        api: Class<Api>
+//    ) : Api {
+//        return Retrofit.Builder()
+//            .baseUrl(BASE_URL)
+//            .client(OkHttpClient.Builder().also { client ->
+//                if (BuildConfig.DEBUG) {
+//                    val logging = HttpLoggingInterceptor()
+//                    logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+//                    client.addInterceptor(logging)
+//                }
+//            }.build())
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//            .create(api)
+//    }
 
 }
