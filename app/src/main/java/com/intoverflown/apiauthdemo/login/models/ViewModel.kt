3c9a8b4c9ced9559ccf646bfe30c2467ede7f9b3
RocqJones <jonesmbindyo@gmail.com>
2021-04-27 16:11:58 +0300

    Old: ViewModel:

--- app/src/main/java/com/intoverflown/apiauthdemo/login/models/ViewModel.kt
+++ app/src/main/java/com/intoverflown/apiauthdemo/login/models/ViewModel.kt
@@ -1,22 +1,28 @@
 package com.intoverflown.apiauthdemo.login.models
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.intoverflown.apiauthdemo.data.LoginResponse
-import com.intoverflown.apiauthdemo.network.ApiResponseHandler
-import com.intoverflown.apiauthdemo.repository.AuthRepository
-import kotlinx.coroutines.launch
+//class ViewModel(private val authRepository: AuthRepository) : ViewModel() {
+//
+//    private val myResponse : MutableLiveData<ApiResponseHandler<LoginResponse>> = MutableLiveData()
+//    val loginResponse : LiveData<ApiResponseHandler<LoginResponse>> get() = myResponse
+//
+//    fun login(username : String, password : String) {
+//        viewModelScope.launch {
+//            myResponse.value= authRepository.login(username, password)
+//        }
+//    }
+//}
 
-class ViewModel(private val authRepository: AuthRepository) : ViewModel() {
+// 2nd Test
 
-    val myResponse : MutableLiveData<ApiResponseHandler<LoginResponse>> = MutableLiveData()
-    val loginResponse : LiveData<ApiResponseHandler<LoginResponse>> get() = myResponse
-
-    fun login(username : String, password : String) {
-        viewModelScope.launch {
-            myResponse.value= authRepository.login(username, password)
-        }
-    }
-}
+//class ViewModel(private val repository : Repository) : ViewModel() {
+//
+//
+//    val myResponse : MutableLiveData<Call<LoginResponse>> = MutableLiveData()
+//
+//    fun login(username: String, password: String) {
+//        viewModelScope.launch {
+//            val response = repository.login(username, password)
+//            myResponse.value = response
+//        }
+//    }
+//}
