d36157a63f8c775857e471edb07bc48fe04cfa2b
RocqJones <jonesmbindyo@gmail.com>
2021-04-28 13:44:06 +0300

    Login updated: Success

--- app/src/main/java/com/intoverflown/apiauthdemo/login/LoginActivity.kt
+++ app/src/main/java/com/intoverflown/apiauthdemo/login/LoginActivity.kt
@@ -1,54 +1,134 @@
 package com.intoverflown.apiauthdemo.login
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import com.android.volley.*
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.intoverflown.apiauthdemo.MainActivity
+import com.intoverflown.apiauthdemo.data.LoginResponse
+import com.intoverflown.apiauthdemo.data.SharedPreferenceManager
 import com.intoverflown.apiauthdemo.databinding.ActivityLoginBinding
-import com.intoverflown.apiauthdemo.login.models.ViewModel
-import com.intoverflown.apiauthdemo.login.models.ViewModelFactory
-import com.intoverflown.apiauthdemo.network.ApiClient
-import com.intoverflown.apiauthdemo.network.ApiInterface
-import com.intoverflown.apiauthdemo.network.ApiResponseHandler
-import com.intoverflown.apiauthdemo.repository.AuthRepository
+import com.intoverflown.apiauthdemo.utils.Constants
+import org.json.JSONException
+import org.json.JSONObject
+
 
 class LoginActivity : AppCompatActivity() {
 
-    private var binding : ActivityLoginBinding? = null
-    private lateinit var viewModel: ViewModel
+    private var TAG = "LoginActivity- "
+    private var binding: ActivityLoginBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
 
-        val viewModelFactory = ViewModelFactory(AuthRepository(ApiClient().buildApi(ApiInterface::class.java)))
-        viewModel = ViewModelProvider(this, viewModelFactory).get(ViewModel::class.java)
-
-        binding!!.loginBtn.setOnClickListener {
-            val username = binding!!.username.text.toString().trim()
-            val password = binding!!.password.text.toString().trim()
-
-            viewModel.login(username, password)
-
-            viewModel.loginResponse.observe(this, Observer {
-                when (it) {
-                    is ApiResponseHandler.Success -> {
-                        // print results
-                        Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
-                        val intent = Intent(this, MainActivity::class.java)
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                        startActivity(intent)
-                    }
-
-                    is ApiResponseHandler.Failure -> {
-                        Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            })
+        /** User is logged in */
+        if (SharedPreferenceManager.getInstance(this).isLoggedIn()) {
+            finish()
+            startActivity(Intent(this, MainActivity::class.java))
         }
+
+        /** Call login method */
+        binding!!.loginBtn.setOnClickListener {
+            // Endpoint
+            val url = Constants.BASE_URL + "Connect/Login"
+            // user login
+            loginUser(url)
+        }
+    }
+
+    private fun loginUser(url: String) {
+        val queue = Volley.newRequestQueue(applicationContext)
+        val jsonObject = JSONObject()
+
+        // get user credentials
+        val username = binding!!.username.text.toString().trim()
+        val password = binding!!.password.text.toString().trim()
+
+        if (username.isNotEmpty() && password.isNotEmpty()) {
+            val pd = ProgressDialog(this)
+            pd.setMessage("Logging in user!\nPlease wait...")
+            pd.setProgressStyle(ProgressDialog.STYLE_SPINNER)
+            pd.show()
+
+            try {
+                jsonObject.put("username", username)
+                jsonObject.put("password", password)
+
+                Log.d(TAG + "LoginUser", jsonObject.toString())
+
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+
+            // connect
+            val jsonObjectRequest: JsonObjectRequest = object : JsonObjectRequest(Method.POST,
+                    url, jsonObject, Response.Listener { response ->
+                try {
+                    Log.d("Connection ", response.toString())
+                    // Toast.makeText(applicationContext, response.toString(), Toast.LENGTH_LONG).show()
+                    pd.hide()
+
+                    //creating a new user object
+                    val user = LoginResponse(
+                            response.getString("Id"),
+                            response.getString("FirstName"),
+                            response.getString("LastName"),
+                            response.getString("Username"),
+                            response.getString("Role"),
+                            response.getString("Token")
+                    )
+                    // Store the user in shared pref
+                    Log.d("token b4 SharedPref", user.Token)
+
+                    SharedPreferenceManager.getInstance(applicationContext).userLogin(user)
+
+                    Log.d("token in SharedPref", user.Token)
+
+                    Toast.makeText(applicationContext, "Successful", Toast.LENGTH_SHORT).show()
+
+                    intentHome()
+
+                } catch (e: Exception) {
+                    Toast.makeText(this, Log.getStackTraceString(e) + "",
+                            Toast.LENGTH_LONG).show()
+                    Log.i(TAG, Log.getStackTraceString(e))
+                }
+            }, Response.ErrorListener { error ->
+                Log.e("error", error.toString())
+                Toast.makeText(applicationContext, "Login failed!", Toast.LENGTH_SHORT).show()
+                pd.hide()
+            }) {
+                @Throws(AuthFailureError::class)
+                override fun getHeaders(): Map<String, String> {
+                    val params: MutableMap<String, String> = HashMap()
+                    params["Content-Type"] = "application/json"
+                    return params
+                }
+
+                override fun getBodyContentType(): String {
+                    return "application/json"
+                }
+            }
+
+            jsonObjectRequest.retryPolicy = DefaultRetryPolicy(
+                    60000,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT)
+            queue.add(jsonObjectRequest)
+        }
+    }
+
+    private fun intentHome() {
+        val i = Intent(applicationContext, MainActivity::class.java)
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(i)
+        finish()
     }
 }
