1a2981dac50e05f3facc3a5095b9c2db66ec9552
RocqJones <jonesmbindyo@gmail.com>
2021-04-28 13:49:23 +0300

    Retrofit cleanup: rm repository

--- app/src/main/java/com/intoverflown/apiauthdemo/repository/AuthRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.intoverflown.apiauthdemo.repository
-
-//class AuthRepository(private val api : ApiInterface) : BaseRepository() {
-//
-//    // login
-//    suspend fun login(username: String, password : String) = safeApiCall {
-//        api.login(username, password)
-//    }
-//}
